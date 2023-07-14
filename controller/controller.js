28155a83db6b98b3c9afb854995d7611c90290df
Ensar Yesir <yesirensar@gmail.com>
2023-07-14 10:29:45 +0300

    middleware update2

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,10 @@
 var path=require('path');
 
 module.exports.index = function(req,res){
+    console.log(req.deneme); //middleware ile console a yazıyı çağırdım
     res.sendFile(path.join(__dirname,'../view/index.html'));
 };
+
 
 module.exports.login = function(req,res){
     res.sendFile(path.join(__dirname,'../view/login.html'));
--- router/router.js
+++ router/router.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var controller=require('../controller/controller');
 
+router.use(function(req, res, next){
+    req.deneme = 'middleware deneme';
+    next(); //middleware oluşturursak bir sonrakine geçiş için next kullanmamız lazım
+});
+
 router.get('/home',controller.index);
 router.get('/login',controller.login);
 router.get('/AddAppointment',controller.AddAppointment);
