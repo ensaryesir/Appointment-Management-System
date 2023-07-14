b1b9a782adc06e60f1ff40c0f6375d9f70fd3779
Ensar Yesir <yesirensar@gmail.com>
2023-07-14 10:46:23 +0300

    update controller.js

--- app_server/controller/controller.js
+++ app_server/controller/controller.js
@@ -2,23 +2,23 @@ var path=require('path');
 
 module.exports.index = function(req,res){
     console.log(req.deneme); //middleware ile console a yazıyı çağırdım
-    res.sendFile(path.join(__dirname,'../view/index.html'));
+    res.sendFile(path.join(__dirname,'../../view/index.html'));
 };
 
 
 module.exports.login = function(req,res){
-    res.sendFile(path.join(__dirname,'../view/login.html'));
+    res.sendFile(path.join(__dirname,'../../view/login.html'));
 };
 
 module.exports.AddAppointment = function(req,res){
-    res.sendFile(path.join(__dirname,'../view/AddAppointment.html'));
+    res.sendFile(path.join(__dirname,'../../view/AddAppointment.html'));
 };
 
 module.exports.UpdateAppointment = function(req,res){
-    res.sendFile(path.join(__dirname,'../view/UpdateAppointment.html'));
+    res.sendFile(path.join(__dirname,'../../view/UpdateAppointment.html'));
 };
 
 module.exports.DeleteAppointment = function(req,res){
-    res.sendFile(path.join(__dirname,'../view/DeleteAppointment.html'));
+    res.sendFile(path.join(__dirname,'../../view/DeleteAppointment.html'));
 };
 
