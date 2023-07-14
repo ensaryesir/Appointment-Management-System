eb61b9e4ab0f13fbced7f85339c77ca59a7461b5
Ensar Yesir <yesirensar@gmail.com>
2023-07-14 10:24:14 +0300

    middleware update

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,7 @@
 var path=require('path');
 
 module.exports.index = function(req,res){
+    console.log(req.deneme); //middleware ile console a yazıyı çağırdım
     res.sendFile(path.join(__dirname,'../view/index.html'));
 };
 
