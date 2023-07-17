d406bc647b7ac56ea3ca86aef42cf89c33d7df32
Ensar Yesir <yesirensar@gmail.com>
2023-07-17 11:55:43 +0300

    july 17th update

--- app_server/controller/controller.js
+++ app_server/controller/controller.js
@@ -1,26 +1,28 @@
 var path=require('path');
 
 module.exports.index = function(req,res){
-    //console.log(req.deneme); //middleware ile console a yazıyı çağırdım
     //res.sendFile(path.join(__dirname,'../../view/index.html'));
     res.render('index');
 };
 
-
 module.exports.login = function(req,res){
-    res.sendFile(path.join(__dirname,'../../view/login.html'));
+    //res.sendFile(path.join(__dirname,'../../view/login.html'));
+    res.render('login');
 };
 
 module.exports.AddAppointment = function(req,res){
-    res.sendFile(path.join(__dirname,'../../view/AddAppointment.html'));
+    //res.sendFile(path.join(__dirname,'../../view/AddAppointment.html'));
+    res.render('AddAppointment');
 };
 
 module.exports.UpdateAppointment = function(req,res){
-    res.sendFile(path.join(__dirname,'../../view/UpdateAppointment.html'));
+    //res.sendFile(path.join(__dirname,'../../view/UpdateAppointment.html'));
+    res.render('UpdateAppointment');
 };
 
 module.exports.DeleteAppointment = function(req,res){
-    res.sendFile(path.join(__dirname,'../../view/DeleteAppointment.html'));
+    //res.sendFile(path.join(__dirname,'../../view/DeleteAppointment.html'));
+    res.render('DeleteAppointment');
 };
 
 module.exports.layout = function(req,res){
